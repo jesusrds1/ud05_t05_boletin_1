e7bc7572c8ca687b467bc6068972784e3fcd748e
jesusrds1 <jesusrds09@gmail.com>
2022-01-19 14:25:11 +0100

    Ejercicio 2

---
+++ ejercicio_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOLETIN 2 EJERCIO</title>
+</head>
+<body>
+    <?php
+
+echo "Calle Comercio n1 Sober"."<br/>"; 
+echo "603767919";
+
+    ?>
+    
+</body>
+</html>
