555c779a353be4a4ee531746ae75dc247ab4c0de
jesusrds1 <jesusrds09@gmail.com>
2022-01-19 14:14:09 +0100

    Ejercicio 1

--- ejercicio_1.php
+++ ejercicio_1.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BOLETIN 1 EJERCIO</title>
 </head>
 <body>
+    <?php
+    echo "Utiliza código PHP"
+    ?>
     
 </body>
 </html>
